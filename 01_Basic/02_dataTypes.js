68cac58c216ae0247455ae31e94011c3537dca05
Chitra Bahadur Thapa Chhetri <geevanthapa750@gmail.com>
2024-11-13 12:29:19 +0000

    DataTypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a newer version.
+// alart(3+3) //we are using nodeJs, not browser
+console.log(3
+    +3);  //code readability should be high.
+
+    console.log("Chitra")
+
+
+    //number => 2 the power 53
+    //bigint
+    //string = ""  write in double  quotation
+    //boolen = true/false
+    //null = standalone value
+    //undifined
+    //symbol => unique
+
+    //object
+
+    console.log(typeof undefined);  //undefined
+    console.log(typeof null);  //object
+    
